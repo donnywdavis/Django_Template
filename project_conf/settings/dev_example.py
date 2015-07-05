2318139a8af7911e6854adcb99f222b6b6642031
Donny Davis <donnywdavis@icloud.com>
2015-07-05 16:22:04 -0400

    Initial commit

---
+++ project_conf/settings/dev_example.py
@@ -0,0 +1,15 @@
+from .common import BASE_DIR
+import os
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
