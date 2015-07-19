770f1bdce813267d053311cfd929cb6fb90bf3e3
Donny Davis <donnywdavis@icloud.com>
2015-07-19 16:42:58 -0400

    Set up index url patterns for static pages app

--- static_pages/views.py
+++ static_pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    This is the route for the home page of the site.
+
+    :param request: Data passed in from the browser
+    :return: Render the template to the browser.
+    """
+
+    return render(request, 'index.html', {})
